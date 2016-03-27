c639f31feee6fa5ef3b188925901f6cd2c6e5e68
xin tong <exinton@gmail.com>
2016-03-27 11:43:25 -0500

    Update VoiceTool.java

--- src/splibraries/VoiceTool.java
+++ src/splibraries/VoiceTool.java
@@ -22,48 +22,57 @@ public class VoiceTool implements ReceiveStreamListener {
   private Player player=null;
   private AudioFormat af=null;
 
+  
 
 public void startMedia(String peerIP,int peerPort,int recvPort, int fmt) {
 
   try{
     //We obtain the DataSource correponding to the microphone capture
-<<<<<<< HEAD
+
     AudioFormat df=new AudioFormat(AudioFormat.LINEAR,8000,16,1);
-=======
-    AudioFormat df=new AudioFormat(AudioFormat.LINEAR,8000,8,1);
->>>>>>> 231d03309614ae5f1c20fc42e1aff67e04ef4b72
     Vector devices=CaptureDeviceManager.getDeviceList(df);
     CaptureDeviceInfo di=(CaptureDeviceInfo) devices.elementAt(0);
-    DataSource daso=Manager.createDataSource(di.getLocator());
 
+    DataSource daso=Manager.createDataSource(di.getLocator());
+    Format wantedFormat=di.getFormats()[1];
+    System.out.println(di.getFormats()[1]);
+    if (requestCaptureFormat(wantedFormat,daso) == false){
+    	System.out.println("Could not change the audio format to LINEAR, 8000.0 Hz, 16-bit, Mono, LittleEndian, Signed ");
+    }
+    
    //We process the DataSource in order to change it into a streamable format
    myProcessor = Manager.createProcessor(daso);
    myProcessor.configure();
    while (myProcessor.getState()!=Processor.Configured) {
-   Thread.sleep(10);
+   Thread.sleep(20);
   }
+   System.out.println("start process2");
    myProcessor.setContentDescriptor(new ContentDescriptor(ContentDescriptor.RAW_RTP));
    TrackControl track[] = myProcessor.getTrackControls();
+   Format mySupportedFormats[]=track[0].getSupportedFormats();
+   
+   
+   
    switch (fmt) {
-<<<<<<< HEAD
-     case 3: af=new AudioFormat(AudioFormat.GSM,8000,4,1);
+
+     case 3: af=new AudioFormat(AudioFormat.MPEG_RTP,44100,16,1);
      		break;
-     case 4: af=new AudioFormat(AudioFormat.G723_RTP,8000,4,1);
+     case 4: af=new AudioFormat(AudioFormat.G723_RTP,8000,16,1);
      		break;
-     case 5: af=new AudioFormat(AudioFormat.LINEAR,44100,16,2);
+     case 5: af=new AudioFormat(AudioFormat.ULAW_RTP,8000,8,1);
      	break;		
      		
    }
    track[0].setFormat(af);
-   
-=======
-     case 3: af=new AudioFormat(AudioFormat.GSM_RTP,8000,4,1);
-     case 4: af=new AudioFormat(AudioFormat.G723_RTP,8000,4,1);
-   }
-   track[0].setFormat(af);
->>>>>>> 231d03309614ae5f1c20fc42e1aff67e04ef4b72
+   System.out.println("start process3");
    myProcessor.realize();
-   while (myProcessor.getState() != Processor.Realized) {}
+   System.out.println("start process4");
+   
+   while (myProcessor.getState() != Processor.Realized) {
+	   Thread.sleep(50);
+   }
+   
+   System.out.println("start process5");
    DataSource ds = myProcessor.getDataOutput();
 
    //We create a SessionManager
@@ -75,22 +84,19 @@ public void startMedia(String peerIP,int peerPort,int recvPort, int fmt) {
     //We initialize the session
     SessionAddress senderAddr = new SessionAddress();
     myVoiceSessionManager.initSession(senderAddr, null, 0.05, 0.25);
-
+    
     //We construct the arguments to the startSession method from the parameters
     // passed to startMedia() and invoke startSession() on the SessionManager
     InetAddress destAddr = InetAddress.getByName(peerIP);
     SessionAddress localAddr =
         new SessionAddress (InetAddress.getLocalHost(),recvPort,InetAddress.getLocalHost(),recvPort + 1);
     SessionAddress remoteAddr = new SessionAddress(destAddr, peerPort, destAddr, peerPort + 1);
+
+    System.out.println("start process end");
     myVoiceSessionManager.startSession(localAddr , localAddr , remoteAddr,null);
-<<<<<<< HEAD
-htop
-=======
-
->>>>>>> 231d03309614ae5f1c20fc42e1aff67e04ef4b72
-
+ 
    // We obtain a SendStream from the Datasource obtained as output to the processor
-   ss = myVoiceSessionManager.createSendStream(ds, 0);
+    ss = myVoiceSessionManager.createSendStream(ds, 0);
 
    //We start capture and transmission
    ss.start();
@@ -144,5 +150,24 @@ htop
       player.start();
     }
   }
-}
+  
+  public boolean requestCaptureFormat(Format requested_format, DataSource ds) {
 
+      if (ds instanceof CaptureDevice) {
+          FormatControl[] fcs = ((CaptureDevice) ds).getFormatControls();
+          for (FormatControl fc : fcs) {
+              Format[] formats = ((FormatControl) fc).getSupportedFormats();
+              for (Format format : formats) {
+                  if (requested_format.matches(format)) {
+                      ((FormatControl) fc).setFormat(format);
+                      return true;
+                  }
+              }
+          }
+      }
+      return false;
+  }
+  
+
+  
+}
