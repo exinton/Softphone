7462cc46d9ee2fbea151c52ed63c28595e9e7580
exinton <exinton@gmail.com>
2016-04-14 23:37:48 -0500

    commit

---
+++ src/sipproxy_secure/SipproxySecureApp.java
@@ -0,0 +1,58 @@
+package sipproxy_secure;
+
+import javax.swing.UIManager;
+import java.awt.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2007</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class SipproxySecureApp {
+  boolean packFrame = false;
+
+  //Construct the application
+  public SipproxySecureApp() {
+    SipproxySecureGUI frame = new SipproxySecureGUI();
+    //Validate frames that have preset sizes
+    //Pack frames that have useful preferred size info, e.g. from their layout
+    if (packFrame) {
+      frame.pack();
+    }
+    else {
+      frame.validate();
+    }
+    //Center the window
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension frameSize = frame.getSize();
+    if (frameSize.height > screenSize.height) {
+      frameSize.height = screenSize.height;
+    }
+    if (frameSize.width > screenSize.width) {
+      frameSize.width = screenSize.width;
+    }
+    frame.setLocation((screenSize.width - frameSize.width) / 2, (screenSize.height - frameSize.height) / 2);
+    frame.setVisible(true);
+  }
+
+  //Main method
+  public static void main(String[] args) {
+	// setup TLS properties
+      System.setProperty( "javax.net.ssl.keyStore",  "/home/exinton/git/Softphone/src/tls/xintong.jks" );
+      System.setProperty( "javax.net.ssl.trustStore","/home/exinton/git/Softphone/src/tls/xintong.jks");
+      System.setProperty( "javax.net.ssl.keyStorePassword", "tgq588" );
+      System.setProperty( "javax.net.ssl.keyStoreType", "jks" );
+	  
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+    new SipproxySecureApp();
+  }
+}
